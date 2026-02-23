484fc73230670c40cda92ed9d06352701dca756b
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2026-02-23 14:11:17 +0100

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1318,8 +1318,6 @@ async function processMatch(homeTeam, awayTeam, gameState) {
                 player.form = Math.max(40, player.form - Math.floor(Math.random() * 3));
             }
             
-            // Generar posibles lesiones
-            generateInjury(player);
         });
         
         // Actualizar popularidad
